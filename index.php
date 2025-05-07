6b218981c3f6fad85a12352ebf974afbf5b3df9a
shevaosyana <234muhammadrafli@gmail.com>
2025-05-08 04:28:11 +0700

    comit 1
    tahap awal tampilan  login untuk masuk tampilan dan perbaiki database yang eror

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+session_start();
+require_once 'config/database.php';
+
+// Check if user is logged in
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+// Get counts for dashboard
+$stmt = $pdo->query("SELECT COUNT(*) FROM posts");
+$postCount = $stmt->fetchColumn();
+
+$stmt = $pdo->query("SELECT COUNT(*) FROM categories");
+$categoryCount = $stmt->fetchColumn();
+
+$stmt = $pdo->query("SELECT COUNT(*) FROM users");
+$userCount = $stmt->fetchColumn();
+
+include 'includes/header.php';
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Dashboard</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-lg-4 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3><?php echo $postCount; ?></h3>
+                            <p>Posts</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-file-alt"></i>
+                        </div>
+                        <a href="posts.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-4 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3><?php echo $categoryCount; ?></h3>
+                            <p>Categories</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-list"></i>
+                        </div>
+                        <a href="categories.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-4 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3><?php echo $userCount; ?></h3>
+                            <p>Users</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-users"></i>
+                        </div>
+                        <a href="users.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+            </div>
+            <!-- /.row -->
+        </div>
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php include 'includes/footer.php'; ?> 
